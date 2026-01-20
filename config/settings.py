525442d2737105ab8959578fdc72fa798a55345b
Dastan <dastan.kubanuulu1@gmail.com>
2026-01-20 22:38:00 +0700

    add uuid field serializerg

--- config/settings.py
+++ config/settings.py
@@ -155,7 +155,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
-    ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    ]
 }
